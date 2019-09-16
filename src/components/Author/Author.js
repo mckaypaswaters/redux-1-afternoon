ba0bb5102b41d3f3d4c896017ce497e9d2694b6c
McKay Waters <mckaypaswaters@gmail.com>
2019-09-16 16:24:25 -0600

    finished

--- src/components/Author/Author.js
+++ src/components/Author/Author.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import './Author.css';
+import store, {UPDATE_AUTHOR_FIRST_NAME, UPDATE_AUTHOR_LAST_NAME} from '../../store'
 
 class Author extends Component {
   constructor(props) {
     super(props);
+    const reduxState = store.getState()
     this.state = {
-      authorFirst: '',
-      authorLast: ''
+      authorFirst: reduxState.authorFirstName,
+      authorLast: reduxState.authorLastName
     };
   }
 
@@ -24,6 +26,14 @@ class Author extends Component {
   }
   saveChanges() {
     // Send data to Redux state
+    store.dispatch({
+      type: UPDATE_AUTHOR_FIRST_NAME,
+      payload: this.state.authorFirst
+    })
+    store.dispatch({
+      type: UPDATE_AUTHOR_LAST_NAME,
+      payload: this.state.authorLast
+    })
   }
   render() {
     return (
--- src/store.js
+++ src/store.js
@@ -1,1 +1,54 @@
+import {createStore} from 'redux'
 
+// INITIAL STATE
+const initialState = {
+    name: '',
+    category: '',
+    authorFirstName: '',
+    authorLastName: '',
+    ingredients: [],
+    instructions: [],
+    recipes: []
+}
+
+// ACTION CONSTANTS
+export const UPDATE_NAME = 'UPDATE_NAME'
+export const UPDATE_CATEGORY = 'UPDATE_CATEGORY'
+export const UPDATE_AUTHOR_FIRST_NAME = 'UPDATE_AUTHOR_FIRST_NAME'
+export const UPDATE_AUTHOR_LAST_NAME = 'UPDATE_AUTHOR_LAST_NAME'
+export const ADD_INGREDIENT = 'ADD_INGREDIENT'
+export const ADD_INSTRUCTION = 'ADD_INSTRUCTION'
+export const ADD_RECIPE = 'ADD_RECIPE'
+
+// REDUCER FUNCTION
+function reducer(state = initialState, action) {
+    switch(action.type){
+        case UPDATE_NAME:
+            return {...state, name: action.payload}
+        case UPDATE_CATEGORY:
+            return {...state, category: action.payload}
+        case UPDATE_AUTHOR_FIRST_NAME:
+            return {...state, authorFirstName: action.payload}
+        case UPDATE_AUTHOR_LAST_NAME:
+            return {...state, authorLastName: action.payload}
+        case ADD_INGREDIENT:
+            const newIngredients = [...state.ingredients, action.payload]
+            return {...state, ingredients: newIngredients}
+        case ADD_INSTRUCTION:
+            const newInstructions = [...state.instructions, action.payload]
+            return {...state, instructions: newInstructions}
+        case ADD_RECIPE:
+            const {name, category, authorFirstName, authorLastName, ingredients, instructions} = state
+            const recipe = {name, category, authorFirstName, authorLastName, ingredients, instructions}
+            const newRecipe = [...state.recipes, recipe]
+            return {...state, recipes: newRecipe}
+        default:
+            return state
+    }
+}
+
+// CREATE STORE
+export default createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
